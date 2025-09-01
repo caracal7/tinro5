cffed519d3e8da3935966cf8afb71c59a181be14
kotik <dimkasv1@gmail.com>
2025-09-01 23:24:32 +0300

    Bugfix

--- src/meta.svelte.js
+++ src/meta.svelte.js
@@ -39,16 +39,38 @@ Meta.subscribe(newMeta => {
 });
 
 
-let pendingMeta = null;
+const initialState = {
+    from: undefined,
+    url: undefined,
+    query: undefined,
+    match: undefined,
+    pattern: undefined,
+    breadcrumbs: undefined,
+    params: undefined,
+    subscribe: undefined
+};
+
+let pendingMeta = undefined;
 let updateScheduled = false;
 
 const applyMetaUpdate = () => {
     updateScheduled = false;
-    if(pendingMeta) metaStore.set(pendingMeta);
+    if (pendingMeta !== undefined) {
+        const currentMeta = Meta.get();
+        const newMeta = pendingMeta || initialState;
+        if(JSON.stringify(currentMeta) !== JSON.stringify(newMeta)) {
+            metaStore.set(newMeta);
+        }
+    }
+    pendingMeta = undefined;
 };
 
 export function updateMeta(newMeta) {
+    if (pendingMeta && !newMeta) {
+        return;
+    }
     pendingMeta = newMeta;
+
     if (!updateScheduled) {
         updateScheduled = true;
         Promise.resolve().then(applyMetaUpdate);
